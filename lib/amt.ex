97f07ba1dc5ef04048fcbfac8325a3e5fc2ebc89
Muharem Hrnjadovic <mh@cloud1.it>
2015-05-06 09:17:57 +0200

    name extraction works

--- lib/amt.ex
+++ lib/amt.ex
@@ -5,9 +5,24 @@ defmodule Amt do
   """
 
   def aname(email) do
+    email = clean_utfs(email)
     { :ok, rx } = Regex.compile(~S"You have received an application for .+ from (.+)\s+View", "ums")
     nl = Regex.run(rx, email) |> List.last |> String.split |> Enum.map &String.capitalize/1
     Enum.join(nl, " ")
+  end
+
+  def clean_utfs(txt) do
+    txt = Regex.replace(~R/=\n/, txt, "", [:global])
+    utfs = Regex.scan(~r/(=[0-9a-fA-F]{2})+/, txt) |> Enum.map(&List.first/1)
+    do_clean_utfs(utfs, txt)
+  end
+
+  def do_clean_utfs([], txt), do: txt
+
+  def do_clean_utfs([utf|remaining_utfs], txt) do
+    rune = String.split(utf, "=", trim: true) |> Enum.map(fn x -> String.to_integer(x, 16) end) |> :erlang.list_to_binary
+    txt = String.replace(txt, utf, rune)
+    do_clean_utfs(remaining_utfs, txt)
   end
 
 end
--- test/amt_test.exs
+++ test/amt_test.exs
@@ -23,11 +23,25 @@ defmodule AmtTest do
   test "applicants's name is extracted correctly 3" do
     ts = """
       Hi Joe,
-      You have received an application for IT security expert from Oliver N=C3=B6=
-      then
+      You have received an application for IT security expert from Gulliver J=C3=B6=
+      llo
       View all applicants: https://www.linkedin.com/e/v2?e=3D4vz24-i9aageqh-1t&am=
       """
-    assert Amt.aname(ts) == "Oliver Nöthen"
+    assert Amt.aname(ts) == "Gulliver Jöllo"
   end
 
+  test "clean_utfs deals with the utf-8 bytes correctly" do
+    ts = """
+      Gulliver J=C3=B6=
+      llo
+      View
+      """
+    assert Amt.clean_utfs(ts) == "Gulliver Jöllo\nView\n"
+  end
+
+  test "utf-8 bytes handling with 0+ occurrences" do
+    assert Amt.do_clean_utfs([], "expected") == "expected"
+    assert Amt.do_clean_utfs(["=c3=b6"], "J=c3=b6llo") == "Jöllo"
+    assert Amt.do_clean_utfs(["=c3=b6", "=c3=bb"], "J=c3=b6llo=c3=bb") == "Jölloû"
+  end
 end
